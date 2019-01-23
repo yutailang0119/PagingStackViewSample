3b3addf9062bef54c19ad2088a74984b1f6638c0
yutailang0119 <muta.yutaro@gmail.com>
2019-01-23 19:05:52 +0900

    Initial Commit

---
+++ PagingStackViewSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PagingStackViewSample
+//
+//  Created by Yutaro Muta on 2019/01/23.
+//  Copyright © 2019 yutailang0119. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
